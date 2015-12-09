4a29a47e3736d7b7a600334b10277baba6e89228
Mackenzie Straight <eiz@e7m.org>
2015-12-08 23:06:00 -0500

    Add symbol exports.

--- package.lisp
+++ package.lisp
@@ -1,2 +1,6 @@
 (defpackage #:cl-soda
-  (:use #:cl #:cffi #:named-readtables #:optima))
+  (:use #:cl #:cffi #:named-readtables #:optima)
+  (:export
+   #:make-signing-keypair
+   #:crypto-sign #:crypto-sign-open #:make-box-keypair #:crypto-box-precompute
+   #:crypto-box #:nonce-increment #:make-zero-nonce #:crypto-box-open))
